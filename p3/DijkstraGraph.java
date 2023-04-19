a5655e06529cdd8114420583ab10b90e583d1b5c
Nico Salm <nico@salm.dev>
2023-04-19 17:54:37 -0500

    p3 changes

--- p3/DijkstraGraph.java
+++ p3/DijkstraGraph.java
@@ -1,16 +1,28 @@
 // --== CS400 File Header Information ==--
-// Name: <your full name>
-// Email: <your @wisc.edu email address>
-// Group and Team: <your group name: two letters, and team color>
-// Group TA: <name of your group's ta>
-// Lecturer: <name of your lecturer>
-// Notes to Grader: <optional extra notes>
+// Name: Nico Salm
+// Email: nbsalm
+// Group and Team: AC blue
+// Group TA: Rachit
+// Lecturer: Dahl
+// Notes to Grader: NONE
 
 import java.util.PriorityQueue;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+// junit testing
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 /**
  * This class extends the BaseGraph data structure with additional methods for
@@ -137,38 +149,192 @@ public class DijkstraGraph<NodeType, EdgeType extends Number>
      * @return the cost of the shortest path between these nodes
      */
     public double shortestPathCost(NodeType start, NodeType end) {
-        if (!nodes.containsKey(start) || !nodes.containsKey(end)) { // check if the start and end nodes are in the graph
-            throw new NoSuchElementException("Start or end node not found");
-        }
-        SearchNode endNode = computeShortestPath(start, end); // find the end node of the shortest path
-        return endNode.cost; // return the cost of the shortest path
+        return computeShortestPath(start, end).cost; // return the cost of the shortest path
     }
 
-    // TODO: implement 3+ tests in step 8.
+    /**
+     * JUnit test for DijkstraGraph class which tests computeShortestPath
+     */
+    @Test
+    public void testComputeShortestPath() {
+
+        var graph = new DijkstraGraph<String, Double>();
+        var nodes = new String[] { "A", "B", "D", "E", "F", "G", "H", "I", "L", "M", "X" };
+        // Add the nodes to the graph
+        Arrays.stream(nodes).forEach(node -> graph.insertNode(node));
+        // create an array of edges to add to the graph
+        var edges = new String[][] {
+                { "A", "H", "8.0" },
+                { "A", "B", "1.0" },
+                { "A", "M", "5.0" },
+                { "B", "M", "3.0" },
+                { "D", "A", "7.0" },
+                { "D", "G", "2.0" },
+                { "F", "G", "9.0" },
+                { "G", "L", "7.0" },
+                { "H", "B", "6.0" },
+                { "H", "I", "2.0" },
+                { "I", "H", "2.0" },
+                { "I", "D", "1.0" },
+                { "I", "L", "5.0" },
+                { "M", "E", "3.0" },
+                { "M", "F", "4.0" },
+                // note that Node X is not connected to any other nodes
+        };
+        // Add the edges to the graph
+        Arrays.stream(edges).forEach(edge -> graph.insertEdge(edge[0], edge[1], Double.parseDouble(edge[2])));
+
+        // Test the shortest path from D to I
+        var path = graph.computeShortestPath("D", "I");
+
+        assertEquals("I", path.node.data); // check that the end node is indeed I
+        assertEquals(17.0, path.cost); // check that the cost is correct (17.0)
+
+        // Test the shortest path from A to L
+        path = graph.computeShortestPath("A", "L");
+
+        assertEquals("L", path.node.data); // check that the end node is indeed L
+        assertEquals(15.0, path.cost); // check that the cost is correct (15.0)
+
+        // Test the shortest path from A to G
+        path = graph.computeShortestPath("A", "G");
+        assertEquals(13.0, path.cost); // check that the cost is correct (13.0)
+
+        // Tests exception when start node or end node is not in the graph
+        assertThrows(NoSuchElementException.class, () -> graph.computeShortestPath("Z", "G"));
+        assertThrows(NoSuchElementException.class, () -> graph.computeShortestPath("A", "Z"));
+
+        // Tests exception when start node or end node is null
+        assertThrows(NullPointerException.class, () -> graph.computeShortestPath(null, "G"));
+        assertThrows(NullPointerException.class, () -> graph.computeShortestPath("A", null));
+
+        // Tests exception when there is no path from start node to end node
+        assertThrows(NoSuchElementException.class, () -> graph.computeShortestPath("G", "X"));
+    }
+
+    /**
+     * JUnit test for the DijkstraGraph class which tests shortestPathData
+     */
+    @Test
+    public void testShortestPathData() {
+
+        var graph = new DijkstraGraph<String, Double>();
+        var nodes = new String[] { "A", "B", "D", "E", "F", "G", "H", "I", "L", "M" };
+        // Add the nodes to the graph
+        Arrays.stream(nodes).forEach(node -> graph.insertNode(node));
+        // create an array of edges to add to the graph
+        var edges = new String[][] {
+                { "A", "H", "8.0" },
+                { "A", "B", "1.0" },
+                { "A", "M", "5.0" },
+                { "B", "M", "3.0" },
+                { "D", "A", "7.0" },
+                { "D", "G", "2.0" },
+                { "F", "G", "9.0" },
+                { "G", "L", "7.0" },
+                { "H", "B", "6.0" },
+                { "H", "I", "2.0" },
+                { "I", "H", "2.0" },
+                { "I", "D", "1.0" },
+                { "I", "L", "5.0" },
+                { "M", "E", "3.0" },
+                { "M", "F", "4.0" }
+        };
+        // Add the edges to the graph
+        Arrays.stream(edges).forEach(edge -> graph.insertEdge(edge[0], edge[1], Double.parseDouble(edge[2])));
+
+        // Test the shortest path from D to I
+        var path = graph.shortestPathData("D", "I");
+        var expected = new String[] { "D", "A", "H", "I" };
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], path.get(i)); // expect D, A, H, I
+        }
+
+        // Test the shortest path from A to L
+        path = graph.shortestPathData("A", "L");
+        expected = new String[] { "A", "H", "I", "L" };
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], path.get(i)); // expect A, H, I, L
+        }
+
+        // Test the shortest path from A to G
+        path = graph.shortestPathData("A", "G");
+        expected = new String[] { "A", "H", "I", "D", "G" };
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], path.get(i)); // expect A, H, I, D, G
+        }
+
+        // Tests exception when start node or end node is not in the graph
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathData("Z", "G"));
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathData("A", "Z"));
+
+        // Tests exception when start node or end node is null
+        assertThrows(NullPointerException.class, () -> graph.shortestPathData(null, "G"));
+        assertThrows(NullPointerException.class, () -> graph.shortestPathData("A", null));
+
+        // Tests exception when there is no path from start node to end node
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathData("G", "X"));
+
+    }
+
+    /**
+     * JUnit test for the DijkstraGraph class which tests shortestPathCost
+     */
+    @Test
+    public void testShortestPathCost() {
+        var graph = new DijkstraGraph<String, Double>();
+        var nodes = new String[] { "A", "B", "D", "E", "F", "G", "H", "I", "L", "M" };
+        // Add the nodes to the graph
+        Arrays.stream(nodes).forEach(node -> graph.insertNode(node));
+        // create an array of edges to add to the graph
+        var edges = new String[][] {
+                { "A", "H", "8.0" },
+                { "A", "B", "1.0" },
+                { "A", "M", "5.0" },
+                { "B", "M", "3.0" },
+                { "D", "A", "7.0" },
+                { "D", "G", "2.0" },
+                { "F", "G", "9.0" },
+                { "G", "L", "7.0" },
+                { "H", "B", "6.0" },
+                { "H", "I", "2.0" },
+                { "I", "H", "2.0" },
+                { "I", "D", "1.0" },
+                { "I", "L", "5.0" },
+                { "M", "E", "3.0" },
+                { "M", "F", "4.0" }
+        };
+        // Add the edges to the graph
+        Arrays.stream(edges).forEach(edge -> graph.insertEdge(edge[0], edge[1], Double.parseDouble(edge[2])));
+
+        var path = graph.shortestPathCost("D", "I");
+        assertEquals(17.0, path); // expect 10.0
+
+        // Test the shortest path from A to L
+        path = graph.shortestPathCost("A", "L");
+        assertEquals(15.0, path); // expect 15.0
+
+        // Test the shortest path from A to G
+        path = graph.shortestPathCost("A", "G");
+        assertEquals(13.0, path);
+
+        // Test the shortest path from A to B
+        path = graph.shortestPathCost("A", "B");
+        assertEquals(1.0, path); // expect 1.0
+
+        // Tests exception when start node or end node is not in the graph
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathCost("Z", "G"));
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathCost("A", "Z"));
+
+        // Tests exception when start node or end node is null
+        assertThrows(NullPointerException.class, () -> graph.shortestPathCost(null, "G"));
+        assertThrows(NullPointerException.class, () -> graph.shortestPathCost("A", null));
+
+        // Tests exception when there is no path from start node to end node
+        assertThrows(NoSuchElementException.class, () -> graph.shortestPathCost("G", "X"));
+
+    }
 
     public static void main(String[] args) {
-        DijkstraGraph<String, Double> graph = new DijkstraGraph<String, Double>();
-        graph.insertNode("A");
-        graph.insertNode("B");
-        graph.insertNode("C");
-        graph.insertNode("D");
-        graph.insertNode("E");
-        graph.insertNode("F");
-
-        graph.insertEdge("B", "E", 1.0);
-        graph.insertEdge("B", "C", 2.0);
-        graph.insertEdge("C", "B", 3.0);
-        graph.insertEdge("C", "F", 1.0);
-        graph.insertEdge("F", "D", 1.0);
-        graph.insertEdge("D", "E", 3.0);
-        graph.insertEdge("E", "A", 4.0);
-        graph.insertEdge("A", "D", 5.0);
-        graph.insertEdge("A", "B", 6.0);
-        graph.insertEdge("F", "A", 1.0);
-        graph.insertEdge("A", "C", 2.0);
-
-        // shortest path from A to G should be A -> C -> F -> G
-
-        System.out.println(graph.shortestPathData("B", "F")); // should print [A, B, C]
     }
 }
